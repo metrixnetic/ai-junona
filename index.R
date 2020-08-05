490aeb67df4de18750f6a7b7ee6a041d57a114cf
Atom230 <54737433+Atom230@users.noreply.github.com>
2020-08-05 10:58:00 +0300

    weather

--- index.R
+++ index.R
@@ -136,6 +136,10 @@ execute_cmd  <- function(cmd, userInput) {
     } else if (cmd == 'myMap') {
         
         browseURL("https://www.google.com/maps", browser="firefox")
+            
+    } else if (cmd == 'weather') {
+        
+        browseURL("https://www.accuweather.com/", browser="firefox")
 
     } else {
         
